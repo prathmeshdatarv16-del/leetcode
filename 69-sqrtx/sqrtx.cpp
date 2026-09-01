c34d1e55a5b338c63d6750fddfc16a76a3dc6977
prathmeshdatarv16-del <prathmeshdatarv16@gmail.com>
2026-09-01 23:43:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.40%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        if (x == 0 || x == 1) return x;
+        
+        int left = 1;
+        int right = x;
+        int ans = 0;
+        
+        // Binary Search
+        while (left <= right) 
+        {
+           
+            int mid = left + (right - left) / 2;
+            
+            
+            long long square = (long long)mid * mid;
+            
+            if (square == x) 
+            {
+                return mid; 
+            } 
+            else if (square < x) 
+            {
+                
+                ans = mid;
+                left = mid + 1;
+            } 
+            else 
+            {
+                
+                right = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
