689421d4772cbcd4616b57145999585b6788e21e
prathmeshdatarv16-del <prathmeshdatarv16@gmail.com>
2026-09-06 23:46:58 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (34.86%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    // Helper function to do the actual recursive traveling
+    void traverse(TreeNode* node, vector<int>& result) {
+        if (node == nullptr) {
+            return;
+        }
+        
+        traverse(node->left, result);
+        result.push_back(node->val);
+        traverse(node->right, result);
+    }
+
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;          
+        traverse(root, result);      
+        return result;               
+    }
+};
